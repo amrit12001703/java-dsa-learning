d677d4e395bddc9ef022bbbe756540cd1943b208
Amrit Anand <amritanand8271@gmail.com>
2023-07-26 16:40:20 +0530

    For loops

---
+++ JavaCode/src/P006_For_Loops.java
@@ -0,0 +1,10 @@
+public class P006_For_Loops {
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5};
+//        enhanced for loop
+        for(int i:arr)
+            System.out.println(i);
+
+
+    }
+}
