7a10655372926cd276da1fb33903dd3f69bfb896
Amrit Anand <amritanand8271@gmail.com>
2023-08-09 07:07:28 +0530

    Natural Numbers

---
+++ DSA_Questions/src/Recursion/P004_Natural_Number_Sum.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class P004_Natural_Number_Sum {
+
+    public static int sumOfNaturalNum(int n){
+        if(n==0)
+            return 0;
+        return sumOfNaturalNum(n-1)+n;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(sumOfNaturalNum(n));
+
+    }
+}
