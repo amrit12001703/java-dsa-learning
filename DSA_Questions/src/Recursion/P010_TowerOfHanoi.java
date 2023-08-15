92c251fe5b1d1f48b503cb4b1e081b1bcedf4721
Amrit Anand <amritanand8271@gmail.com>
2023-08-15 11:37:48 +0530

    Binary Search

---
+++ DSA_Questions/src/Recursion/P010_TowerOfHanoi.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class P010_TowerOfHanoi {
+
+    public static void towerOfHanoi(int noOfDisc,String tower1,String tower2, String tower3){
+        if(noOfDisc==1){
+            System.out.println("Move Disc "+noOfDisc+" From "+tower1 +" to "+tower3);
+            return;
+        }
+        towerOfHanoi(noOfDisc-1,tower1,tower3,tower2);
+        System.out.println("Move Disc "+noOfDisc+" From "+tower1 +" to "+tower3);
+        towerOfHanoi(noOfDisc-1,tower2,tower1,tower3);
+    }
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int noOfDisc=sc.nextInt();
+        towerOfHanoi(noOfDisc,"A","B","C");
+    }
+}
--- DSA_Questions/src/Searching/P001_BinarySearch.java
+++ DSA_Questions/src/Searching/P001_BinarySearch.java
@@ -1,8 +1,6 @@
-package Array;
+package Searching;
 
-public class P003_BinarySearch {
-
-    //Iterative
+public class P001_BinarySearch {
     public static int binarySearchIterative(int search,int[] arr,int left,int right) {
         while(left<=right){
             int mid=(right+left)/2;
@@ -19,21 +17,21 @@ public class P003_BinarySearch {
     }
 
     //Recursive
-   public static int binarySearchRecursive(int search,int[] arr,int left,int right) {
-       if (right >= left) {
-           int mid = left + (right - left) / 2;
-           if (arr[mid] == search)
-               return mid;
-           if (arr[mid] > search)
-               return binarySearchRecursive(search, arr, left, mid - 1);
-           return binarySearchRecursive(search, arr, mid + 1, right);
-       }
-       return -1;
-   }
+    public static int binarySearchRecursive(int search,int[] arr,int left,int right) {
+        if (right >= left) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == search)
+                return mid;
+            if (arr[mid] > search)
+                return binarySearchRecursive(search, arr, left, mid - 1);
+            return binarySearchRecursive(search, arr, mid + 1, right);
+        }
+        return -1;
+    }
 
     public static void main(String [] args){
         int [] arr={1,2,3,4,5,6,7};
-        int search=8;
+        int search=9;
 //        System.out.println(binarySearchRecursive(search,arr,0,arr.length-1));
         System.out.println(binarySearchIterative(search,arr,0,arr.length-1));
 
