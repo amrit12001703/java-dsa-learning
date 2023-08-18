8875c88cd5af47b1f084a57ad7e6d7ab937f9e3e
Amrit Anand <amritanand8271@gmail.com>
2023-08-18 14:16:04 +0530

    Quick Sort Algorithm

---
+++ DSA_Questions/src/Sorting/P006_QuickSortAlg.java
@@ -0,0 +1,40 @@
+package Sorting;
+
+public class P006_QuickSortAlg {
+
+    static int partition(int arr[], int l, int h)
+    {
+        int pivot=arr[h];
+        int i=l-1;
+        for(int j=l;j<=h-1;j++){
+            if(arr[j]<pivot){
+                i++;
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        int temp=arr[i+1];
+        arr[i+1]=arr[h];
+        arr[h]=temp;
+        return i+1;
+    }
+
+    static void quickSort(int arr[],int l,int h){
+        if(l<h){
+            int p=partition(arr,l,h);
+            quickSort(arr,l,p-1);
+            quickSort(arr,p+1,h);
+        }
+    }
+
+    public static void main(String[] args){
+        int[] arr = {2,4,8,9,3,10,5};
+
+        int n = arr.length;
+        quickSort(arr,0,n-1);
+
+        for(int x: arr)
+            System.out.print(x+" ");
+    }
+}
